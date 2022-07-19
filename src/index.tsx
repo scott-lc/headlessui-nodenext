db4c931ca60c772ee368fe2e7f89fbfddf0e2eda
Scott Sadler <scott@livecontrol.io>
2022-07-19 14:22:11 -0400

    dev checkpoint

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,36 @@
 import { StrictMode, type ReactElement } from "react";
 import { createRoot } from "react-dom/client";
 import { Listbox } from "@headlessui/react";
+import React, { useState } from "react";
 
-const App = (): ReactElement => (
-  <div>
-    <Listbox />
-  </div>
-);
+const people = [
+  { id: 1, name: "Durward Reynolds", unavailable: false },
+  { id: 2, name: "Kenton Towne", unavailable: false },
+  { id: 3, name: "Therese Wunsch", unavailable: false },
+  { id: 4, name: "Benedict Kessler", unavailable: true },
+  { id: 5, name: "Katelyn Rohan", unavailable: false },
+];
+
+const App = (): ReactElement => {
+  const [selectedPerson, setSelectedPerson] = useState(people[0]);
+
+  return (
+    <Listbox value={selectedPerson} onChange={setSelectedPerson}>
+      <Listbox.Button>{selectedPerson.name}</Listbox.Button>
+      <Listbox.Options>
+        {people.map((person) => (
+          <Listbox.Option
+            key={person.id}
+            value={person}
+            disabled={person.unavailable}
+          >
+            {person.name}
+          </Listbox.Option>
+        ))}
+      </Listbox.Options>
+    </Listbox>
+  );
+};
 
 createRoot(document.querySelector("#root")!).render(
   <StrictMode>
